a0f2311c86c812f898860bfd0b2d9a2c24583869
liyuxin <liyuxin6@xiaomi.com>
2020-07-19 17:29:57 +0800

    feat: 测试2
    测试一下

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 1.0.0 (2020-07-19)
+
+
+### Features
+
+* 测试 ([5f08c16](https://github.com/liyuxin130443/test-git/commit/5f08c162199d02b492eb5ab78fd2d4ce00e4498b))
+
+
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,7 @@
       <el-table-column prop="name" label="姓名" width="180"></el-table-column>
       <el-table-column prop="address" label="地址"></el-table-column>
       <el-table-column prop="address" label="哈哈"></el-table-column>
+      <el-table-column prop="address" label="啦啦"></el-table-column>
     </el-table>
   </div>
 </template>
