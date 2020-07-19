a3e99f3324271062d163d46df74cf0f281a17674
liyuxin <liyuxin6@xiaomi.com>
2020-07-19 18:07:55 +0800

    测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,7 @@
       <el-table-column prop="address" label="啦啦"></el-table-column>
        <el-table-column prop="address" label="啦啦"></el-table-column>
        <el-table-column prop="address" label="啦啦"></el-table-column>
+       <el-table-column prop="address" label="啦啦"></el-table-column>
     </el-table>
   </div>
 </template>
